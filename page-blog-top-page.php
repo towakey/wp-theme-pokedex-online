d72db33c0dff67078d87961f0b793c77fc59412f
towakey <kyosgame@gmail.com>
2022-06-19 23:21:21 +0900

    コメントにしてた不要行の削除

--- page-blog-top-page.php
+++ page-blog-top-page.php
@@ -18,28 +18,6 @@ Template Post Type: page
             </div>
         </a>
     </div>
-    <!-- <article class="blog-list__list-item">
-        <a href="<?php the_permalink(); ?>" class="blog-item">
-            <?php // アイキャッチを表示させる start ?>    
-            <div class="blog-item__thumbnail">
-                <?php if(has_post_thumbnail()): ?>
-                <img class="blog-item__thumbnail-image" src="<?php the_post_thumbnail_url('large'); ?>">
-                <?php endif; ?>
-            </div>
-            <?php // アイキャッチを表示させる end ?>
-            <div class="blog-item__content">
-                <?php // タイトルを表示させる start ?>
-                <h3 class="blog-item__title"><?php the_title(); ?></h3>
-                <?php // タイトルを表示させる end ?>
-                <?php // 抜粋を表示させる start ?>
-                <h3 class="blog-item__read"><?php the_excerpt(); ?></h3>
-                <?php // 抜粋を表示させる end ?>
-                <div class="blog-item__button">
-                    <span class="blog-item__button-more">記事を読む</span>
-                </div>
-            </div>
-        </a>
-    </article> -->
 <?php endwhile; endif; ?>
 <?php wp_reset_query(); ?>
 <!-- contents end -->
